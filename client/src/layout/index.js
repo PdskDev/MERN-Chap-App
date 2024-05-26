9df297e2d2276325a2ac1cb759d9292b22ea318e
Nadet LAMBI-BIDZIMOU <nadetdev@gmail.com>
2024-05-26 17:50:13 +0200

    Add images asset + header layout

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "../assets/logoChat-clean1.png";
 
 const AuthLayout = ({ children }) => {
   return (
     <>
-      <div>Logo</div>
+      <header className="flex justify-center items-center py-3 h-20 shadow-md">
+        <img src={logo} alt="Chat App Logo" width={100} height={20} />
+      </header>
       {children}
     </>
   );
