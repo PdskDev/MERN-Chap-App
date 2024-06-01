0539ea9a1c4fbc08460dd39c6246128ca721651f
Nadet LAMBI-BIDZIMOU <nadetdev@gmail.com>
2024-06-01 19:01:38 +0200

    User details - start

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
+  const getUserDetailsInfo = async () => {
+    try {
+      const url_api_user_info = `${process.env.REACT_APP_BACKEND_URL}/api/user-info`;
+      const response = await axios({
+        url: url_api_user_info,
+        method: "get",
+        withCredentials: true,
+      });
+      console.log("current user", response.data?.data);
+      toast.success(response?.data?.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    getUserDetailsInfo();
+  }, []);
+
   return (
     <div>
       HomePage
